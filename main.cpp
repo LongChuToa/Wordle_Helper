4847e34ef6e2a43de88c3ce8ab9e15e8b18b7fe5
Hoang Long <ghoulkysinh184@gmail.com>
2025-06-22 20:59:29 +0700

    del some file

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "base_game/base_game.h"
-#include <ctime>
-
-int main() {
-	Base_Game demo = Base_Game("wordlist.txt");
-	srand(time(0));
-	int seed = (rand() % 682 + 317) * (rand() % 682 + 317);
-	demo.play_with_choice_word("crawl", true);
-}
